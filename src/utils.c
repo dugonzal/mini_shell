38b31b53d5a1a7c354876d00c060061d19d2ffcd
dugonzal <dugonzal@gmail.com>
2023-04-10 21:39:36 +0000

    si no puedo obtener el usuario, el nombre del usuario es anonimo

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 21:26:30 by ciclo             #+#    #+#             */
-/*   Updated: 2023/04/10 21:33:20 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/04/10 21:38:30 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,11 @@ char	*prompt(void)
 	char	*usuario;
 	char	*anfitrion;
 
-	usuario = getenv("USER");
-	if (usuario)
-		usuario = ft_strjoin("\033[1;32m", usuario, 0);
+	if (!(usuario = getenv("USER")))
+		usuario = ft_strdup("\033[1;31mAnonimo\033[0m");
+	usuario = ft_strjoin("\033[1;31m", usuario, 0);
 	anfitrion = ft_strjoin("\033[1;34m@minishell> \033[0m", " ", 0);
-	if (!usuario)
-		prompt = anfitrion;
-	else
-		prompt = ft_strjoin(usuario, anfitrion, 0);
+	prompt = ft_strjoin(usuario, anfitrion, 0);
 	free(usuario);
 	free (anfitrion);
 	return (prompt);

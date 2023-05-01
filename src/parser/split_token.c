23c58f05c902d6681ebe1136604cd697702b0c2b
dugonzal <ciclcontinuo2@gmail.com>
2023-05-01 19:27:22 +0000

    actualizo split token reduzco lineas y lo hago mas claro, para mi al menos

--- src/parser/split_token.c
+++ src/parser/split_token.c
@@ -6,24 +6,16 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 09:03:08 by ciclo             #+#    #+#             */
-/*   Updated: 2023/04/30 15:23:14 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/01 19:26:10 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// voy a quitar las comilla y comprobar que hayan dos
-//No interpretar comillas sin cerrar o caracteres
-//especiales no especificados en el
-//enunciado como \ (barra invertida) o ; (punto y coma).
-// voy hacer primero el caso en el que los caracteres
-// entre comillas no tienen
-// espacios luego con espacio
 char **split_token(char *prompt, char *set, char *specials, char *quotes)
 {
   char **tmp;
   int word;
-  int size;
 
   word = 0;
   tmp = (char **)malloc(sizeof(char *)\
@@ -31,26 +23,18 @@ char **split_token(char *prompt, char *set, char *specials, char *quotes)
   fun_check(*tmp);
   while (*prompt)
   {
-	size = 0;
 	if (*prompt && search(specials, *prompt)) // specials_token
-	{ // reducir lineas metiendo direcatmente la funcion del size de la lina a strndup, pero tendria que avanzar hasta ese punnto
-		size = specials_token(prompt);
-		tmp[word++] = fun_check(ft_strndup(prompt, size));
-	}
+		tmp[word++] = fun_check(ft_strndup(prompt, specials_token(prompt)));
 	else if (*prompt && !search(specials, *prompt) // caracteres_token
 	&& !search(quotes, *prompt) && !search(set, *prompt))
-	{
-	  size = caracteres_token(prompt, set, quotes, specials);
-	  tmp[word++] = fun_check(ft_strndup(prompt, size));
-	}
+	  tmp[word++] = fun_check(ft_strndup(prompt, caracteres_token(prompt, set, quotes, specials)));
 	else if (*prompt && search(quotes, *prompt))
-	{
-	  size = quotes_token(prompt, *prompt);
-	  tmp[word++] = fun_check(ft_strndup(prompt, size));
-	}
+	  tmp[word++] = fun_check(ft_strndup(prompt, quotes_token(prompt, *prompt)));
 	else
 		prompt++;
-	prompt += size;
+	prompt += ft_strlen(tmp[word - 1]);
+	if (*prompt && search(set, *prompt))
+	  prompt++;
   }
   tmp[word] = NULL;
   return (tmp);

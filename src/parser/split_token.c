b5c0d4add6397248f3dec9a1371524ed53209a99
dugonzal <ciclcontinuo2@gmail.com>
2023-05-01 19:32:30 +0000

    consigo llegar a las 26 lineas, ahora a ver si el largo de la lineas cumple la norma xd

--- src/parser/split_token.c
+++ src/parser/split_token.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 09:03:08 by ciclo             #+#    #+#             */
-/*   Updated: 2023/05/01 19:26:10 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/01 19:30:54 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,17 @@ char **split_token(char *prompt, char *set, char *specials, char *quotes)
   fun_check(*tmp);
   while (*prompt)
   {
-	if (*prompt && search(specials, *prompt)) // specials_token
+	if (*prompt && search(specials, *prompt))
 		tmp[word++] = fun_check(ft_strndup(prompt, specials_token(prompt)));
-	else if (*prompt && !search(specials, *prompt) // caracteres_token
+	else if (*prompt && !search(specials, *prompt)
 	&& !search(quotes, *prompt) && !search(set, *prompt))
-	  tmp[word++] = fun_check(ft_strndup(prompt, caracteres_token(prompt, set, quotes, specials)));
+	  tmp[word++] = fun_check(ft_strndup(prompt,\
+	caracteres_token(prompt, set, quotes, specials)));
 	else if (*prompt && search(quotes, *prompt))
 	  tmp[word++] = fun_check(ft_strndup(prompt, quotes_token(prompt, *prompt)));
-	else
-		prompt++;
 	prompt += ft_strlen(tmp[word - 1]);
 	if (*prompt && search(set, *prompt))
-	  prompt++;
+		prompt++;
   }
   tmp[word] = NULL;
   return (tmp);

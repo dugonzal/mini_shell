7118499bb09d17880977ccf2950df4c736d5b77a
dugonzal <ciclcontinuo2@gmail.com>
2023-05-03 17:36:51 +0000

    empiezo la creacion de tester con bash

---
+++ tester.sh
@@ -0,0 +1,30 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    tester.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/05/03 17:27:23 by Dugonzal          #+#    #+#              #
+#    Updated: 2023/05/03 17:34:42 by Dugonzal         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+make
+
+function tester() {
+	test=$(echo $@"; exit" | ./minishell 2>&-)
+	test2=$(echo $@"; exit" | bash 2>&-)
+	if [ test != test2 ]; then
+		echo "Error: $1"
+		echo "Expected: $test2"
+		echo "Got: $test"
+		exit 1
+	else
+		echo "OK:"
+	fi
+}
+
+tester "echo hello world"

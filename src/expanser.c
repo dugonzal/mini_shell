9df9603e01154292d06163684189ccd051a44e9c
dugonzal <ciclcontinuo2@gmail.com>
2023-05-03 20:56:37 +0000

    la funcion de expancion funciona bien expande las variables de entorno, si la variable de entotrno pasada por el usuario no es correcta retorno mensaje de error, y tiene la opcion de volver a intentar

--- src/expanser.c
+++ src/expanser.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:03:30 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/05/03 20:24:43 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/03 20:53:30 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,31 +30,37 @@ char *ft_getenv(char *str)
 	return (getenv(str));
   return (NULL);
 }
-// "'hola '$USER' que tal'"
-// antes de la expansion -> durante la expansion -> despues de la expansion
+
 int	expanser(t_data *data)
 {
   int	i;
   int	j;
-  char *env;
-  char	*full;
+  char	*env;
+  int	size;
+  
   i = -1;
   if (!data->bufer)
 	return (0);
-  (void)full;
   while (data->bufer[++i])
 	if (data->bufer[i] && search(data->bufer[i], '$'))
 	{
-	  j = 0;
+	  j = 1;
 	  while (data->bufer[i][j] != '$')
 		  j++;
 	  if (data->bufer[i][j] == '$' && ft_isalpha(data->bufer[i][j + 1]))
 	  {
-		env = ft_getenv(strndup(&data->bufer[i][j + 1], count_expanser(&data->bufer[i][j + 1]))); // expanser
+		j++;
+		size = count_expanser(&data->bufer[i][j]);
+		env = ft_getenv(strndup(&data->bufer[i][j], size)); // expanser
+		if (!env)
+		{
+		  err_msg(RED"Error: Environment variable not found."RESET);
+		  return (1);
+		}
 		if (j > 1)
-			env = ft_strjoin(strndup(data->bufer[i], j), env, 0);
-		if (data->bufer[i][j + count_expanser(&data->bufer[i][j + 1])] != 0)
-			env = ft_strjoin(env, &data->bufer[i][j + count_expanser(&data->bufer[i][j + 1])], 0);
+			env = ft_strjoin(strndup(data->bufer[i], j - 1), env, 1);
+		if (data->bufer[i][j + size] != 0)
+			env = ft_strjoin(env, &data->bufer[i][j + size], 1);
 		free(data->bufer[i]);
 		data->bufer[i] = env;
 		if (search(data->bufer[i], '$'))
@@ -62,7 +68,6 @@ int	expanser(t_data *data)
 	  }
 	  else if (data->bufer[i][j + 1] != ft_isalpha(data->bufer[i][j + 1]))
 		continue ;
-	  // estoy en el array de la expansion
 	}
   return (0);
 }

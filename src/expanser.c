33328112681d9303302bf9bba7b9fc5408ff5f09
dugonzal <ciclcontinuo2@gmail.com>
2023-05-03 20:27:14 +0000

    funciona hago una recursiva para expandir si hay mas de una variable para expandir en el mismo array

--- src/expanser.c
+++ src/expanser.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:03:30 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/05/03 20:06:33 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/03 20:24:43 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,12 @@ int	expanser(t_data *data)
 {
   int	i;
   int	j;
+  char *env;
   char	*full;
   i = -1;
   if (!data->bufer)
 	return (0);
+  (void)full;
   while (data->bufer[++i])
 	if (data->bufer[i] && search(data->bufer[i], '$'))
 	{
@@ -48,14 +50,15 @@ int	expanser(t_data *data)
 		  j++;
 	  if (data->bufer[i][j] == '$' && ft_isalpha(data->bufer[i][j + 1]))
 	  {
-		full = ft_strjoin(ft_strndup(&data->bufer[i][j - 1], j), ft_getenv(strndup(&data->bufer[i][j + 1], count_expanser(&data->bufer[i][j + 1]))), 0); // guardo la expansion
-		printf ("----%s---\n", full);
-		  //if (data->bufer[i][j + count_expanser(&data->bufer[i][j + 1])] != 0) // si hay algo despues de la expansion
-		 // char *tmp1 = ft_strdup(&data->bufer[i][j + count_expanser(&data->bufer[i][j + 1])]); // guardo lo que hay despues de la expansion
-		//full = ft_strjoin(full, tmp1, 1); // concateno lo que hay antes de la expansion con la expansion y lo que hay despues de la expansion
-		printf ("----%s---\n", full);
-		//if (search(data->bufer[i], '$'))
-		//  expanser(data);
+		env = ft_getenv(strndup(&data->bufer[i][j + 1], count_expanser(&data->bufer[i][j + 1]))); // expanser
+		if (j > 1)
+			env = ft_strjoin(strndup(data->bufer[i], j), env, 0);
+		if (data->bufer[i][j + count_expanser(&data->bufer[i][j + 1])] != 0)
+			env = ft_strjoin(env, &data->bufer[i][j + count_expanser(&data->bufer[i][j + 1])], 0);
+		free(data->bufer[i]);
+		data->bufer[i] = env;
+		if (search(data->bufer[i], '$'))
+		  expanser(data);
 	  }
 	  else if (data->bufer[i][j + 1] != ft_isalpha(data->bufer[i][j + 1]))
 		continue ;

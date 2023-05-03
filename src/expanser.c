112b6d8bc94bd5990a4eed5e3f94b4933572d6b4
dugonzal <ciclcontinuo2@gmail.com>
2023-05-03 19:33:41 +0000

    empiezo a programar la logica de la expancion

--- src/expanser.c
+++ src/expanser.c
@@ -6,10 +6,58 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:03:30 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/05/03 17:04:29 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/03 19:31:51 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+int count_expanser(char *str)
+{
+  int i;
+
+  i = 0;
+  while (str[i] && ft_isalpha(str[i]))
+	i++;
+  return (i);
+}
+
+char *ft_getenv(char *str)
+{
+  if (!str)
+	return (NULL);
+  if (getenv(str))
+	return (getenv(str));
+  return (NULL);
+}
+
+int	expanser(t_data *data)
+{
+  int	i;
+  int	j;
+
+  i = -1;
+  if (!data->bufer)
+	return (0);
+  while (data->bufer[++i])
+	if (data->bufer[i] && search(data->bufer[i], '$'))
+	{
+	  j = 0;
+	  while (data->bufer[i][j] != '$')
+		  j++;
+	  if (data->bufer[i][j] == '$' && ft_isalpha(data->bufer[i][j + 1]))
+	  {
+		j++;
+		char *tmp = strndup(&data->bufer[i][j], count_expanser(&data->bufer[i][j]));
+		printf ("tmp = [%s]\n", tmp);
+		char *env = ft_getenv(tmp);
+		printf ("----%s---\n", env);
+	  }
+	  else if (data->bufer[i][j + 1] != ft_isalpha(data->bufer[i][j + 1]))
+		continue ;
+	  // estoy en el array de la expansion
+	}
+  return (0);
+}
 
 

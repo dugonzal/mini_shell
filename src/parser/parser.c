f44e2d41106dd2b64498afffff1ebaddc10cd570
dugonzal <ciclcontinuo2@gmail.com>
2023-05-04 16:05:27 +0000

    estoy haciendo pruebas para controlar que hay en el final del vector, si es un |,;,0

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 21:15:13 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/05/04 15:34:51 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:02:35 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	size_argv(char **str)
   i = -1;
   while (str[++i] && !search("|;", str[i][0]))
 	;
-  return (i);
+  return (i + 1);
 }
 
 
@@ -33,21 +33,25 @@ int	parser(t_data *data)
 {
   t_cmd	*cmd;
   int	size;
-  //int	i;
+  int	i;
 
   cmd = (t_cmd *)malloc(sizeof(t_cmd));
+  if (!cmd)
+	return (1);
   size = size_argv(data->bufer);
   cmd->size = size;
-  //cmd->cmd = (char **)ft_calloc(sizeof(char *), size + 1);
-  //i = 0;
-  //while (i < size)
-  //{
-//	cmd->cmd[i] = (data->bufer[i]);
-//	i++;
- // 
-  // }
-  //cmd->cmd[i] = NULL;
- // print (cmd->cmd);
- printf ("size = %d\n", cmd->size);
+  cmd->cmd = (char **)malloc(sizeof(char *) * size + 1);
+  if (!cmd->cmd)
+	return (1);
+  i = 0;
+  while (i < size)
+  {
+	cmd->cmd[i] = (data->bufer[i]);
+	i++;
+  }
+  cmd->cmd[i] = NULL;
+  for (int i = 0; cmd->cmd[i] ; i++)
+	printf ("[%s] ", cmd->cmd[i]);
+  printf (" [%s]", cmd->cmd[i]);
   return (0);
 }

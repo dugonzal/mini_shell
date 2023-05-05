e107402302875020383f20ae2534f34b31952e4c
dugonzal <ciclcontinuo2@gmail.com>
2023-05-05 19:04:29 +0000

    status

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,12 +6,16 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 14:04:21 by ciclo             #+#    #+#             */
-/*   Updated: 2023/05/04 15:18:48 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/05 19:04:16 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
+//Exit status:
+//  125  if the env command itself fails
+//  126  if COMMAND is found but cannot be invoked
+//  127  if COMMAND cannot be found
+//  -    the exit status of COMMAND otherwise
 void	ft_exit(t_data  *data)
 {
 	data->status = 0;

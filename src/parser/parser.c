16be47993d12ac986e4e433916e0697038f4ba85
dugonzal <ciclcontinuo2@gmail.com>
2023-05-06 19:58:15 +0000

    "esta guay el doble puntero"

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 21:15:13 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/05/05 18:53:32 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/06 19:56:37 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	size_argv(char **str)
   int	i;
 
   i = 0;
-  while (str[i] && !search("|;", str[i][0]))
+  while (str[i] && !search("|;", str[i - 1][0])) // intento copiar tambienl el type
 	i++;
   printf ("[%d] <--> [%s]\n", i, str[i]);
   return (i);
@@ -55,56 +55,50 @@ t_cmd **last_back(t_cmd **cmd, t_cmd *new)
   return (cmd);
 }
 
-t_cmd *new_cmd(char **str, int size)
+t_cmd	*new_cmd(char **str, int size)
 {
-  t_cmd *cmd;
-  int	i;
+  t_cmd		*cmd;
+  int		i;
 
-  cmd = (t_cmd *)ft_calloc(1, sizeof(t_cmd));
   if (!str)
 	return (NULL);
+  cmd = (t_cmd *)ft_calloc(1, sizeof(t_cmd));
   cmd->cmd = (char **)ft_calloc(size + 1, sizeof(char *));
+  if (cmd->cmd == NULL || cmd == NULL)
+	return (NULL);
   i = 0;
   while (i < size)
   {
 	cmd->cmd[i] = str[i];
 	i++;
   }
+  cmd->cmd[size] = NULL;
   cmd->next = NULL;
   cmd->prev = NULL;
-  cmd->type = type(str[i]);
-  printf ("cmd->type = %d\n", cmd->type);
-  cmd->size = size;
-  printf (" = %d\n", cmd->size);
+ // cmd->type = type(str[i]);
+ // printf ("cmd->type = %d\n", cmd->type);
+  //cmd->size = size;
+  //printf (" = %d\n", cmd->size);
   return (cmd);
 }
 
 // quiero ir metiendo los comandos primero en una arrray y
 // luego a un puntero puntero
+int lista_lists(t_data *data, t_cmd **cmd)
+{
+  (void)data;
+  (void)cmd;
+  return (1);
+}
+
+
 
 int	parser(t_data *data)
 {
   t_cmd	*cmd;
-  int	size;
-  int	i;
-
-  i = 0;
-  cmd = ft_calloc(1, sizeof(t_cmd));
-  while (data->bufer[i])
-  {
-	size = size_argv(&data->bufer[i]);
-	last_back((t_cmd **)cmd->cmd, new_cmd(&data->bufer[i], size));
-	i += size + 1; // mas uno por el ;	
-  }
-	return (1);
-print (cmd->cmd);
- // printf ("%zu %d", arr_size(cmd->cmd), cmd->size);
-  //  if (cmd->cmd[i - 1] == NULL)
-//	cmd->type = 0;
- // else if (cmd->cmd[i - 1][0] == ';')
-//	cmd->type = 1;
-///	else
-//	cmd->type = 2;
- // printf ("cmd->type = %d\n", cmd->type);
+/// necesito varias funcione para pasar los argumentos a la lista enlzadad pero no quiero xd
+  /// muchas funciones hacen que sea maas largo, y literal no hay opcion
+  lista_lists(data, &cmd);
+	return(1);
   return (0);
 }

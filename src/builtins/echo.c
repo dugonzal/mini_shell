2faae0932096cad81c9c6f071ae72e987b8e4a13
dugonzal <ciclcontinuo2@gmail.com>
2023-05-12 12:02:39 +0000

    builtins

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,37 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sizquier <sizquier@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:42:28 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/05/10 20:01:52 by sizquier         ###   ########.fr       */
+/*   Updated: 2023/05/12 11:54:14 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-/*
-int ft_echo(char **str)
-{
-  int i;
-  int flag;
-
-  i = -1;
-  flag = 0;
-  while (str[++i])
-  {
-	if (!ft_strncmp(str[i], "-n", 2))
-	{
-	  flag = 1;
-	  continue ;
-	}
-	ft_printf ("%s", str[i]);
-  }
-  if (!flag)
-	  printf ("\n");
-  return (0);
-} 
-*/
 void	ft_echo_builtin(t_cmd *cmd)
 {
 	char	*flag_n; // para la variable -n

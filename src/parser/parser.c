aa940517ad143b3a31e0bf5401922d3644f35341
dugonzal <ciclcontinuo2@gmail.com>
2023-05-13 16:19:38 +0000

    update parsers

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 21:15:13 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/05/13 14:20:00 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/13 16:16:43 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,24 @@ int parser_cmds(char **bufer, t_cmd **cmd)
   return (size);
 }
 
+void error_fd(t_data *data)
+{
+	perror("dup");
+	data->status = 1; // error
+	return ;
+}
+
 void copy_fd(t_data *data)
 {
   int fd;
 
   fd = dup(STDIN_FILENO);
   if (fd == -1)
-  {
-	perror("dup");
-	data->status = 1; // error
-  }
+	error_fd(data);
   data->file_in = fd;
   fd = dup(STDOUT_FILENO);
   if (fd == -1)
-  {
-	perror("dup");
-	close(data->file_in);
-	data->status = 1; // error
-  }
+	error_fd(data);
   data->file_out = fd;
 }
 
@@ -58,7 +58,7 @@ void reset_fd(t_data *data)
 	if (dup2(data->file_in, STDIN_FILENO) == -1)
 	{
 	  perror("dup2");
-	  data->status = 1; // error
+	  data->status = 1;
 	  close(data->file_in);
 	  close(data->file_out);
 	  return ;
@@ -67,10 +67,10 @@ void reset_fd(t_data *data)
   	if (dup2(data->file_out, STDOUT_FILENO) == -1)
 	{
 	  perror("dup2");
-	  data->status = 1; // error
+	  data->status = 1;
 	  close(data->file_out);
 	  return ;
-	}// copy file descriptor i\o
+	}
 	close (data->file_out);
 }
 
@@ -78,8 +78,8 @@ void exec(t_cmd *cmd, t_data *data)
 {
   t_cmd *tmp;
 
-  tmp = cmd;
   copy_fd(data);
+  tmp = cmd;
   while (tmp)
   {
 	//print(cmd->cmd);

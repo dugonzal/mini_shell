012a0150f589bd59ef50ffc658c322c1fe0ca70b
dugonzal <ciclcontinuo2@gmail.com>
2023-05-17 19:59:34 +0000

    arreglado el problema de las cadenas cuando pasabas varias cadenas entre comillas, y seguido sin espacios otra cadena entre comillas

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 18:07:01 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/05/17 19:15:48 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/05/17 19:57:59 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,35 +74,25 @@ int type(char *str)
 	  return (-1); // no type
 }
 
-char **quite_zero(char **str)
-{
-  return (str);
-}
-
 // echo "hola""hola" -> echo holahola
 void seach_quotes(char **str, char *quotes)
 {
   int i;
+  int j;
   char tmp;
 
   i = -1;
   while (str[++i])
-	if (search(quotes, str[i][0]))
-	{
-	  tmp = str[i][0];
-	  str[i] = ft_strtrim(str[i], &tmp, 1);
-	  if (str[i][0] == '\0')
+  {
+	j = -1;
+	while (str[i][++j])
+	  if (search(quotes, str[i][j]))
 	  {
-		free(str[i]);
-		str[i] = NULL;	
-		str[i] = str[i - 1];
-		if (str[i + 1])
-		{
-		  str[i] = ft_strjoin(str[i], str[i + 1], 1);
-		  str[i - 1] = str[i];
-		  str[i + 1] = NULL;
-		  str[i] = NULL;
-		}
+		tmp = str[i][j];
+		str[i] = ft_strtrim(str[i], &tmp, 1);
+		while (str[i][++j])
+		  if (str[i][j] == tmp)
+			str[i] = ft_strjoin(ft_strndup(str[i], j), &str[i][j + 2], 1);
 	  }
   }
-}
+  }

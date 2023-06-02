6a4b87fe9edeb531e66a2ddb881042debd8c6571
dugonzal <ciclcontinuo2@gmail.com>
2023-06-02 16:46:02 +0000

    corrijo error, cuando open retorna error

--- src/redirect.c
+++ src/redirect.c
@@ -6,67 +6,20 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 13:05:32 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/06/02 16:31:50 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/06/02 16:44:55 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void tmp_re(t_cmd *cmd, char **str, int i,int fd)
-{
-  if (str[i][0] == '<')
-  {
-	  printf ("l");
-	 ft_strdup(str[i + 1]);
-  }
-  else if (str[i][0] == '>')
-  {
-	 printf ("a");
-	 ft_strdup(str[i + 1]);
-  }
-  cmd->io = fd;
-  str[i] = NULL;
-  str[i + 1] = NULL;
-}
-// realmente no se como enfocarlo para que pueda hacer redirecciones multiples
-// en el mismo comando, pero si que se hacerlo para un solo comando
-// no interpetar recciones detro de comillas
-
-
-/*
-int redir(t_cmd *cmd)
-{
-  int i;
-
-  i = -1;
-  while (cmd->cmd[++i])
-  {
-	if (search(cmd->cmd[i], '<') && !search(cmd->cmd[i], '\'') \
-	  && !search(cmd->cmd[i], '\"'))
-	  tmp_re (cmd, cmd->cmd, i, 0);
-	else if (search(cmd->cmd[i], '>') && !search(cmd->cmd[i], '\'') \
-	  && !search(cmd->cmd[i], '\"'))
-	  tmp_re (cmd, cmd->cmd, i, 1);
-	else if (search(cmd->cmd[i], '>') && cmd->cmd[i][1] == '>' && !search(cmd->cmd[i], '\'') \
-	  && !search(cmd->cmd[i], '\"'))
-	{
-	  cmd->fd[1] = ft_open(cmd->cmd[i + 1], 2);
-	  ft_dup2 (cmd->fd, 1);
-	  cmd->cmd[i] = NULL;
-	  cmd->cmd[i + 1] = NULL;
-	  }	
-  }
-  return (0);
-}
-
-*/
-
 int redir_in(t_cmd *cmd, char **str)
 {
-	if (search(str[0], '<') && !str[1])
+	if (search(str[0], '<') && !str[0][1])
 		cmd->fd[0] = ft_open(str[1], 0);
 	else if (search(str[0], '<') && str[0][1] == '<')
 		printf ("heredoc<");
+	if (cmd->fd[0] < 0)
+		return (1);
 	str[0] = NULL;
 	str[1] = NULL;
 	ft_dup2(cmd->fd, 0);
@@ -79,9 +32,12 @@ int redir_out(t_cmd *cmd, char **str)
 		cmd->fd[1] = ft_open(str[1], 1);
 	else if (search(str[0], '>') && str[0][1] == '>')
 		cmd->fd[1] = ft_open(str[1], 2);
+	if (cmd->fd[1] < 0)
+		return (1);
 	str[0] = NULL;
 	str[1] = NULL;
-	ft_dup2(cmd->fd, 1);
+	if (ft_dup2(cmd->fd, 1))
+		return (1);	
 	return (0);
 }
 	// tendria que dividirlo en dos segementos de redireccion
@@ -96,11 +52,14 @@ int redir(t_cmd *cmd)
 	i = -1;
 	while (cmd->cmd[++i])
 		if (search(cmd->cmd[i], '<'))
-			redir_in(cmd, &cmd->cmd[i]);
+			if (redir_in(cmd, &cmd->cmd[i]))
+				return (1);
 	i = -1;
 	while (cmd->cmd[++i])
 		if (search(cmd->cmd[i], '>'))
-			redir_out(cmd, &cmd->cmd[i]);
+			if (redir_out(cmd, &cmd->cmd[i]))
+				return (1);
+
 	return (0);	
 }
 

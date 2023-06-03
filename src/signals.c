35e8ae21685a1b2efead7fa8122ac8dd3cf1d440
dugonzal <ciclcontinuo2@gmail.com>
2023-06-03 11:21:30 +0000

    normi casi pasada en las señales queda retocar un poco

--- src/signals.c
+++ src/signals.c
@@ -6,32 +6,29 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 21:58:16 by ciclo             #+#    #+#             */
-/*   Updated: 2023/05/31 12:14:57 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/06/03 11:21:04 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-
 void	sig_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		if (rl_on_new_line() == -1) // si la nueva linea está creada, no la crees
-			exit(-1); //salgo de la función
+		if (rl_on_new_line() == -1)
+			exit(-1);
 		printf("\n");
-	//	rl_replace_line("", 0); // creame una nueva linea vacia
-		rl_redisplay();// muestramela.
+	//	rl_replace_line("", 0);
+		rl_redisplay();
 	}
 	if (signum == SIGQUIT)
 	{
 		if (rl_on_new_line() == -1)
 			exit(-1);
-		rl_redisplay();//muestrame la linea, no la crees porque sigquit no la necesita.
+		rl_redisplay();
 	}
-
 }
-
 
 void	sig_handler_child_input(int signum)
 {
@@ -42,10 +39,7 @@ void	sig_handler_child_input(int signum)
 		printf ("Quit: 3\n");
 	}
 	else if (signum == SIGINT)
-	{
-	//	g_status = 130;
 		printf("\n");
-	}
 }
 
 void	handler(int sig, siginfo_t *info, void *context)
@@ -54,16 +48,14 @@ void	handler(int sig, siginfo_t *info, void *context)
 	(void)info;
 	sig_handler(sig);
 	sig_handler_child_input(sig);
-
 }
 
-void signals(void)
+void	signals(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
 	sa.sa_sigaction = handler;
 	sa.sa_flags = SA_SIGINFO;
-
 	sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGQUIT, &sa, NULL);
 }

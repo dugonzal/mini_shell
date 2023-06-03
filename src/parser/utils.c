5b4d666ce2d98c0a1627262118b07ffb7f163c77
dugonzal <ciclcontinuo2@gmail.com>
2023-06-03 10:34:51 +0000

    utils pasa normi

--- src/parser/utils.c
+++ src/parser/utils.c
@@ -6,41 +6,41 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 22:05:48 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/06/02 21:10:09 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/06/03 10:34:36 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int ft_open(char *str, int i)
+int	ft_open(char *str, int i)
 {
-	int fd;
+	int	fd;
 
 	fd = 0;
 	if (!i)
-	  fd = open (str, O_RDONLY); //lectura
+		fd = open (str, O_RDONLY);
 	else if (i == 1)
-	  fd = open (str, O_WRONLY | O_CREAT | O_TRUNC, 0644);  //escritura
+		fd = open (str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	else if (i == 2)
-	  fd = open (str, O_WRONLY | O_CREAT | O_APPEND, 0644); //escritura / lectura > al final
+		fd = open (str, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	else if (i == 3)
-	  fd = open (str, O_RDONLY | O_CREAT, 0644); //lectura / escritura
+		fd = open (str, O_RDONLY | O_CREAT, 0644);
 	if (fd == -1)
-	  ft_putendl_fd(RED"Error: No such file or directory"RESET, 2);
+		ft_putendl_fd(RED"Error: No such file or directory"RESET, 2);
 	return (fd);
 }
 
-void free_cmd(t_cmd *cmd)
+void	free_cmd(t_cmd *cmd)
 {
-  t_cmd *tmp;
+	t_cmd	*tmp;
 
-  tmp = NULL;
-  while (tmp)
-  {
-	cmd = tmp;
-	free_array(tmp->cmd);
-	free (tmp);
-	tmp = cmd->next;
-  }
-  cmd = NULL;
+	tmp = NULL;
+	while (tmp)
+	{
+		cmd = tmp;
+		free_array(tmp->cmd);
+		free (tmp);
+		tmp = cmd->next;
+	}
+	cmd = NULL;
 }

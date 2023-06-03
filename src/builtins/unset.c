a0ba7aa268db83529902b4b9b18677e2fb48a61f
dugonzal <ciclcontinuo2@gmail.com>
2023-06-03 10:03:53 +0000

    unset pasada normi

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 17:48:35 by sizquier          #+#    #+#             */
-/*   Updated: 2023/06/03 09:38:09 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/06/03 10:03:39 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,19 @@ int	ft_unset_builtin2(char *cmd, char ***env)
 	j = 0;
 	if (!cmd)
 		return (0);
-	new_env = (char **)ft_calloc(sizeof(char *),(arr_size(*env) + 1));
+	new_env = (char **)ft_calloc(sizeof(char *), (arr_size(*env) + 1));
 	if (!new_env)
 		return (1);
 	cmd2 = ft_strjoin(cmd, "=", 0);
 	i = -1;
-	while ((*env)[++i]) 
+	while ((*env)[++i])
+	{
 		if ((ft_strncmp((*env)[i], cmd2, ft_strlen(cmd2)) != 0) \
 			&& (ft_strncmp((*env)[i], cmd, ft_strlen(cmd) + 1) != 0))
 			new_env[j++] = ft_strdup((*env)[i]);
+	}
 	new_env[j] = NULL;
-	free_array(*env); 
+	free_array(*env);
 	*env = new_env;
 	free(cmd2);
 	return (0);

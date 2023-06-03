7620293c3a92baa61ce32803cb0b69f843cf7126
dugonzal <ciclcontinuo2@gmail.com>
2023-06-03 10:18:37 +0000

    solo falta separar funciones

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,91 +6,90 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 21:15:13 by Dugonzal          #+#    #+#             */
-/*   Updated: 2023/06/02 21:16:00 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/06/03 10:18:18 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int parser_cmds(char **bufer, t_cmd **cmd)
+int	parser_cmds(char **bufer, t_cmd **cmd)
 {
-  int	size;
+	int	size;
 
-  size = size_node(bufer);
-  last_back_node(cmd, new_node(bufer, size));
-  return (size);
+	size = size_node(bufer);
+	last_back_node(cmd, new_node(bufer, size));
+	return (size);
 }
 
-void error_fd(t_data *data)
+void	error_fd(t_data *data)
 {
 	perror(RED"dup"RESET);
 	data->status = 1;
 	return ;
 }
 
-void copy_fd(t_data *data)
+void	copy_fd(t_data *data)
 {
-  int fd;
+	int	fd;
 
-  fd = dup(0);
-  if (fd < 0)
-	error_fd(data);
-  data->fd[0] = fd;
-  fd = dup(1);
-  if (fd < 0)
-	error_fd(data);
-  data->fd[1] = fd;
+	fd = dup(0);
+	if (fd < 0)
+		error_fd(data);
+	data->fd[0] = fd;
+	fd = dup(1);
+	if (fd < 0)
+		error_fd(data);
+	data->fd[1] = fd;
 }
 
-void reset_fd(t_data *data)
+void	reset_fd(t_data *data)
 {
 	if (dup2(data->fd[0], 0) < 0)
 	{
-	  perror(RED"dup2"RESET);
-	  data->status = 1;
-	  close(data->fd[0]);
-	  close(data->fd[1]);
-	  return ;
+		perror(RED"dup2"RESET);
+		data->status = 1;
+		close(data->fd[0]);
+		close(data->fd[1]);
+		return ;
 	}
 	close (data->fd[0]);
-  	if (dup2(data->fd[1], 1) < 0)
+	if (dup2(data->fd[1], 1) < 0)
 	{
-	  perror(RED"dup2"RESET);
-	  data->status = 1;
-	  close(data->fd[0]);
-	  return ;
+		perror(RED"dup2"RESET);
+		data->status = 1;
+		close(data->fd[0]);
+		return ;
 	}
 	close (data->fd[1]);
 }
 
 int	exec(t_cmd *cmd, t_data *data)
 {
-  t_cmd *tmp;
+	t_cmd	*tmp;
 
-  tmp = cmd;
-  copy_fd(data);
-  while (tmp)
-  {
-	if (cmd->type == 5)
-		if (pipe (cmd->pipe) < 0)
-			return(err_msg(RED"Error : pipe"RESET));
-	redir (cmd);
-	seach_quotes(tmp->cmd, "\"\'");
-	if (builtins(tmp, data))
-		;
-	else
-		bin_execute(tmp, data);
-	if (tmp->type == 5)
-		ft_dup2(tmp->pipe, 0);
-	else
-		reset_fd(data);
-	data->status = cmd->status;
-	tmp = tmp->next;
-  }
-  return (0);
+	tmp = cmd;
+	copy_fd(data);
+	while (tmp)
+	{
+		if (cmd->type == 5 && pipe (cmd->pipe) < 0)
+			return (err_msg(RED"Error : pipe"RESET));
+		redir (cmd);
+		seach_quotes(tmp->cmd, "\"\'");
+		if (builtins(tmp, data))
+			;
+		else
+			bin_execute(tmp, data);
+		if (tmp->type == 5)
+			ft_dup2(tmp->pipe, 0);
+		else
+			reset_fd(data);
+		data->status = cmd->status;
+		tmp = tmp->next;
+	}
+	return (0);
 }
 
-int parser(t_data *data)
+int	parser(t_data *data)
 {
 	t_cmd	*cmd;
 	int		i;
@@ -98,15 +97,15 @@ int parser(t_data *data)
 	i = 0;
 	ft_bzero (&cmd, sizeof(cmd));
 	while (data->bufer[i])
-	if (search("|;", data->bufer[i][0]))
-		i++;
-	else if (data->bufer[i])
-		i += parser_cmds(&data->bufer[i], &cmd);
+	{
+		if (search("|;", data->bufer[i][0]))
+			i++;
+		else if (data->bufer[i])
+			i += parser_cmds(&data->bufer[i], &cmd);
+	}
 	free (data->bufer);
-	if (cmd)	
+	if (cmd)
 		exec(cmd, data);
-  free_cmd(cmd);
-  return (0);
+	free_cmd(cmd);
+	return (0);
 }
-
-

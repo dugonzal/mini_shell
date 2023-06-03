0a892c568fa6cee1622d542fd474af9154b17d98
dugonzal <ciclcontinuo2@gmail.com>
2023-06-03 10:38:15 +0000

    minishell ->main pasa normi

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 10:01:34 by ciclo             #+#    #+#             */
-/*   Updated: 2023/06/03 08:49:26 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/06/03 10:38:00 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 // quotes = "\"\'"
 // whitespaces = " \t\v\f\r\n"
 // lexer -> parser -> expanser -> builtins -> bin_execute -> waitpid
-void get_env_and_path(t_data *data, char **env)
+void	get_env_and_path(t_data *data, char **env)
 {
-	int i;
-	char *path;
+	int		i;
+	char	*path;
 
 	data->env = ft_calloc(arr_size(env) + 1, sizeof(char *));
 	if (!data->env)
@@ -33,18 +33,18 @@ void get_env_and_path(t_data *data, char **env)
 int	main(int ac, char **av, char **env)
 {
 	t_data	data;
-	
+
 	if (ac > 1 && arr_size(av) > 1)
-	  err(RED"minishell: too many arguments"RESET);
+		err(RED"minishell: too many arguments"RESET);
 	ft_bzero (&data, sizeof(t_data));
-  	get_env_and_path(&data, env);
+	get_env_and_path(&data, env);
 	data.user = prompt();
 	while (42)
 	{
 		signals();
 		data.line = readline(data.user);
 		if (!data.line)
-			break;
+			break ;
 		else if (lexer(&data))
 			continue ;
 		parser(&data);

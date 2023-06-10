6b7a43a12e6f241a0da09da05c7641fc521dddb0
dugonzal <ciclocontinuo2@gmail.com>
2023-06-10 15:09:59 +0200

    update : solo faltarian las redirecciones

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 14:04:21 by ciclo             #+#    #+#             */
-/*   Updated: 2023/06/10 14:39:33 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/06/10 15:08:41 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,6 @@ int	ft_exit(t_cmd *cmd, t_data *data)
 	else
 		data->status = 0;
 	free_program(cmd, data);
-	ft_printf(GREEN"good bye :-)\n"RESET);
+	ft_printf(GREEN"good bye :(🙂 \n"RESET);
 	exit (data->status);
 }
--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:48:30 by ciclo             #+#    #+#             */
-/*   Updated: 2023/06/10 14:54:42 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/06/10 15:08:49 by Dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ char	*check_access(char *path, char *bin)
 	while (path[++i])
 		tmp[i] = path[i];
 	tmp[i++] = '/';
-	j = 0;
-	while (bin[j])
-		tmp[i++] = bin[j++];
+	j = -1;
+	while (bin[++j])
+		tmp[i++] = bin[j];
 	tmp[i] = 0;
 	if (!access(tmp, F_OK | X_OK))
 		return (tmp);
@@ -76,7 +76,7 @@ int	bin_execute(t_cmd *cmd, t_data *data)
 			if (ft_dup2 (cmd->pipe, 1))
 				return (1);
 		if (builtins(cmd, data))
-			exit(data->status);
+			exit(EXIT_SUCCESS);
 		else if (cmd->cmd[0][0] == '.' || cmd->cmd[0][0] == '/')
 			execute_relative_or_absolute(cmd, data);
 		else
@@ -86,7 +86,7 @@ int	bin_execute(t_cmd *cmd, t_data *data)
 	if (cmd->pid > 0)
 	{
 		waitpid(cmd->pid, &cmd->status, 0);
-		if (cmd->status == 256)
+		if (cmd->status == 256 || cmd->status == 32512)
 			cmd->status = 127;
 	}
 	return (0);
